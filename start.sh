00f8b504c694e1c955e763f75b12f3f1280d43fd
root <root@api-2.jcloud.local>
2016-05-17 09:14:34 +0800

    增加Dockerfile,去掉mysql用户校验,统一采用datahub校验方式

--- start.sh
+++ start.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+
+#替换redis信息
+
+sed -i 's/REDIS_SERVER_IP/'$REDIS_PORT_6379_TCP_ADDR'/g'  ./config/userQuery.conf
+sed -i 's/REDIS_SERVER_PORT/'$REDIS_PORT_6379_TCP_PORT'/g'  ./config/userQuery.conf
+
+
 ./LdpLocation -c ./config/userQuery.conf -n location 
 
 #nohup ./LdpLocation -c ./config/userQuery.conf -n location  /dev/null 2>&1 &
